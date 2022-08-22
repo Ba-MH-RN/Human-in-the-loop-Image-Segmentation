4704e9955b5778e793c3bb12dc9bbd15ccf51ce8
Maesi <63539289+maesi321@users.noreply.github.com>
2022-08-22 15:44:42 +0200

    backend aufgeräumt

--- backend/startShortTesting.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-CALL .\venv\Scripts\activate.bat
-
-::pytest tests/ImageSegmentation/ -v -W ignore::DeprecationWarning
-pytest tests/REST_Server/data/util -v -W ignore::DeprecationWarning
